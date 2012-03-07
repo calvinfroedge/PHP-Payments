d8cdb2b0cf9aacfb2cbe3dce502225beb9ccbd07
Calvin Froedge <calvinfroedge@gmail.com>
2012-03-06 21:04:51 -0600

    Endpoints are no longer needed in authorize.net config since they are now in driver files

--- config/drivers/authorize_net.php
+++ config/drivers/authorize_net.php
@@ -3,8 +3,6 @@
 $config['api_username'] = "25xTC7vJp";
 $config['api_password'] = "3zF64K5BA9a3y6rG";
 //Note: you can sign into the test interface at test.authorize.net
-$config['api_endpoint_test'] = "https://apitest.authorize.net/xml/v1/request.api";
-$config['api_endpoint_production'] = "https://secure.authorize.net/gateway/transact.dll";
 $config['email_customer'] = TRUE;
 
 return $config;
