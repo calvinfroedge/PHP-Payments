145e1ed336732954f6b09af0a9bb383d1073dfc1
Calvin Froedge <calvinfroedge@gmail.com>
2012-03-06 19:44:14 -0600

    Added function to drivers base class to return an associative array of drivers available (useful for ecommerce apps)

--- lib/payment_driver.php
+++ lib/payment_driver.php
@@ -23,4 +23,42 @@ abstract class Payment_Driver
 	 * Parse the Response and then Delegate to the Response Object
 	 */
 	abstract protected function _parse_response($response);
+
+	/**
+	 * Returns a List of Drivers
+	*/
+	public static function get_drivers()
+	{
+		$drivers = array();
+
+		/*
+		* Humanize the Driver Name
+		*/
+		function humanize($segs)
+		{
+			$prepped = array();
+			foreach($segs as $v) $prepped[] = ucfirst($v);
+			return trim(ucfirst(str_replace('Driver', '', implode(' ', $prepped))));
+		}
+
+		/*
+		* Driverize the Driver Name
+		*/
+		function driverize($segs)
+		{
+			foreach($segs as &$v) ucfirst($v);
+			return str_replace('_driver', '', implode('_', $segs));
+		}
+
+		foreach(scandir(__DIR__.'/payment_drivers') as $driver)
+		{
+			if($driver[0] != '.')
+			{
+				$ex = explode('_', str_replace('.php', '', $driver));
+				$drivers[driverize($ex)] = humanize($ex);
+			}
+		}
+
+		return $drivers;
+	}
 }
